ea3b2ae0d384b8dbe4ac7c314536dc8c6b1fae09
sungsu <kdman21@naver.com>
2020-07-25 22:54:26 +0900

    new new files

---
+++ testApp/src/main/java/com/start/springboot/TestController.java
@@ -0,0 +1,13 @@
+package com.start.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index2";
+	}
+}
