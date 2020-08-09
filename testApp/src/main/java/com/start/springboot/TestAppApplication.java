ef86d44274ba77af8a2346d76712583f963d4e04
sungsu <kdman21@naver.com>
2020-08-09 22:00:07 +0900

    0809 app commit

--- testApp/src/main/java/com/start/springboot/TestAppApplication.java
+++ testApp/src/main/java/com/start/springboot/TestAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestAppApplication {
 
 	//start position
+	//test is created by master. after sub
 	public static void main(String[] args) {
 		SpringApplication.run(TestAppApplication.class, args);
 	}
